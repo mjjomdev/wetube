03b7cab73983da461d8f25f95ae8593c6cd0db90
Minjiicho <minji@dable.io>
2020-02-25 00:05:22 +0900

    Day8

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => res.render("Search", { pageTitle: "Search" });
+export const videos = (req, res) => res.render("Videos", { pageTitle: "Videos" });
+export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail" });
+export const deleteVideo = (req, res) => res.render("Delete Video", { pageTitle: "Delete Video" });
